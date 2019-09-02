ce8ac881cc36928d73297a9dd0612d147f9d8d8b
Alex Emelyanov <holyketzer@gmail.com>
2019-09-02 23:13:03 +0300

    Solved 3.1-3.2 in Ruby

---
+++ ruby/spec/03_stacks_and_queues/01_three_in_one.rb
@@ -0,0 +1,91 @@
+# n - one stack size
+class NStacksArray
+  class StackOverflowError < StandardError
+    def initialize(stack)
+      super("Stack ##{stack} is overflown")
+    end
+  end
+
+  attr_reader :stack_size, :stacks_count
+
+  def initialize(stack_size:, stacks_count:)
+    @stack_size = stack_size
+    @arr = Array.new(stacks_count * stack_size)
+
+    # Indexes of head and tail of stack
+    @heads = Array.new(stacks_count)
+    @tails = Array.new(stacks_count)
+
+    stacks_count.times do |i|
+      @heads[i] = @tails[i] = i * stack_size
+    end
+  end
+
+  def size(stack)
+    si = stack - 1
+    @tails[si] - @heads[si]
+  end
+
+  def push(stack, value)
+    if size(stack) < stack_size
+      si = stack - 1
+      index = (@tails[si] += 1)
+      @arr[index] = value
+    else
+      raise StackOverflowError.new(stack)
+    end
+  end
+
+  def pop(stack)
+    if size(stack) > 0
+      si = stack - 1
+      i = @tails[si]
+      @tails[si] -= 1
+      @arr[i]
+    end
+  end
+
+  def peek(stack)
+    if size(stack) > 0
+      @arr[stack -1]
+    end
+  end
+end
+
+RSpec.describe NStacksArray do
+  let(:stack) { described_class.new(stack_size: 5, stacks_count: 3) }
+
+  describe 'push - pop' do
+    it do
+      stack.push(1, 11)
+
+      stack.push(2, 12)
+      stack.push(2, 13)
+
+      stack.push(3, 14)
+      stack.push(3, 15)
+      stack.push(3, 16)
+
+      expect(stack.pop(1)).to eq 11
+      expect(stack.pop(1)).to eq nil
+
+      expect(stack.pop(2)).to eq 13
+      expect(stack.pop(2)).to eq 12
+      expect(stack.pop(2)).to eq nil
+
+      expect(stack.pop(3)).to eq 16
+      expect(stack.pop(3)).to eq 15
+      expect(stack.pop(3)).to eq 14
+      expect(stack.pop(3)).to eq nil
+    end
+  end
+
+  describe 'size overflow' do
+    it do
+      5.times { |i| stack.push(1, i) }
+      expect(stack.size(1)).to eq 5
+
+      expect { stack.push(1, 6) }.to raise_error(described_class::StackOverflowError)
+    end
+  end
+end
---
+++ ruby/spec/03_stacks_and_queues/02_stack_min.rb
@@ -0,0 +1,79 @@
+# n - stack size
+# O(1) for push, pop and min methods
+class StackWithMin
+  class SNode
+    attr_reader :value, :prev, :min
+
+    def initialize(value, prev_min, prev = nil)
+      @value = value
+      @prev = prev
+      @min = value < prev_min ? value : prev_min
+    end
+  end
+
+  def push(value)
+    if @tail
+      @tail = SNode.new(value, @tail.min, @tail)
+    else
+      @tail = SNode.new(value, value)
+    end
+  end
+
+  def pop
+    if @tail
+      value = @tail.value
+      @tail = @tail.prev
+      value
+    end
+  end
+
+  def min
+    @tail&.min
+  end
+
+  def to_s
+    arr = []
+    curr = @tail
+
+    while curr do
+      arr << "v=#{curr.value} m=#{curr.min}"
+      curr = curr.prev
+    end
+
+    '|[' + arr.reverse.map(&:to_s).join(', ') + ']'
+  end
+end
+
+RSpec.describe StackWithMin do
+  let(:stack) { described_class.new }
+
+  describe 'push - min - pop' do
+    it do
+      stack.push(1)
+      expect(stack.min).to eq 1
+
+      stack.push(2)
+      expect(stack.min).to eq 1
+
+      stack.push(0)
+      expect(stack.min).to eq 0
+
+      stack.push(1)
+      expect(stack.min).to eq 0
+
+      expect(stack.pop).to eq 1
+      expect(stack.min).to eq 0
+
+      expect(stack.pop).to eq 0
+      expect(stack.min).to eq 1
+
+      expect(stack.pop).to eq 2
+      expect(stack.min).to eq 1
+
+      expect(stack.pop).to eq 1
+      expect(stack.min).to eq nil
+
+      expect(stack.pop).to eq nil
+    end
+  end
+end
