0f5dbd1aaaedccf085a8ae9d213509a3336946c8
Alex Emelyanov <holyketzer@gmail.com>
2019-09-28 23:00:36 +0300

    Solved 6.9 and 6.10 in Ruby

---
+++ ruby/spec/06_math_and_logic_puzzles/09_100_lockers.rb
@@ -0,0 +1,53 @@
+def hundreed_lockers_a(lockers_open)
+  (2..100).each do |n|
+    i = n - 1
+    while i < 100 do
+      lockers_open[i] = !lockers_open[i]
+      i += n
+    end
+  end
+
+  lockers_open
+end
+
+def hundreed_lockers_b(lockers_open)
+  (2..100).each do |n|
+
+    primes = to_prime_numbers(n)
+
+    lockers_open[n - 1] = primes.uniq.count % 2 == 0
+  end
+
+  lockers_open
+end
+
+def to_prime_numbers(n)
+  n
+  res = []
+  i = 2
+
+  while i <= n do
+    if n % i == 0
+      res << i
+    end
+
+    i += 1
+  end
+
+  res
+end
+
+RSpec.describe 'hundreed_lockers' do
+  %i(a b).each do |implementation|
+    describe "#{implementation} case" do
+      subject { send("hundreed_lockers_#{implementation}", lockers_open) }
+
+      let(:lockers_open) { 100.times.map { true } }
+
+      it do
+
+        expect(subject.count { |v| v == true }).to eq 10
+      end
+    end
+  end
+end
---
+++ ruby/spec/06_math_and_logic_puzzles/10_poison.rb
@@ -0,0 +1,107 @@
+class SodaBottle
+  def initialize(poisoned)
+    @poisoned = poisoned
+  end
+
+  def drop_to(test_strip)
+    test_strip.drop_from(self, @poisoned)
+  end
+end
+
+class TestStrip
+  attr_reader :positive, :bottles
+
+  def initialize
+    @positive = false
+    @bottles = []
+  end
+
+  def drop_from(bottle, poisoned)
+    @positive ||= poisoned
+    @bottles << bottle
+  end
+
+  def reset_bottles
+    @bottles = []
+  end
+end
+
+def find_poisoned_bottle_a(bottles, test_strips, steps = 1)
+  puts "bottles=#{bottles.size} test_strips=#{test_strips.size} #{steps}"
+
+  if bottles.size <= test_strips.size + 1
+    test_strips.each_with_index do |test_strip, i|
+      test_strip.reset_bottles
+      bottles[i].drop_to(test_strip)
+    end
+
+    bottle = test_strips.find { |s| s.positive == true }&.bottles&.first
+    bottle ||= bottles.last # Bottle without test strip
+
+    {
+      bottle: bottle,
+      steps: steps
+    }
+  else
+    larger_part_size = bottles.size / test_strips.size
+    smaller_part_size = (bottles.size - larger_part_size) / test_strips.size
+    # puts "l=#{larger_part_size} s=#{smaller_part_size}"
+
+    offset = 0
+
+    test_strips.each do |test_strip|
+      test_strip.reset_bottles
+
+      smaller_part_size.times do |i|
+        bottles[i + offset].drop_to(test_strip)
+      end
+
+      offset += smaller_part_size
+    end
+
+    if (test_strip = test_strips.find { |s| s.positive == true })
+      find_poisoned_bottle_a(test_strip.bottles, test_strips.select { |s| s.positive == false }, steps + 1)
+    else
+      find_poisoned_bottle_a(bottles[offset, larger_part_size], test_strips, steps + 1)
+    end
+  end
+end
+
+RSpec.describe 'find_poisoned_bottle' do
+  %i(a).each do |implementation|
+    describe "#{implementation} case" do
+      subject { send("find_poisoned_bottle_#{implementation}", bottles, test_strips) }
+
+      let(:bottles) { 1000.times.map { |i| SodaBottle.new(i == poisoned) } }
+      let(:test_strips) { 10.times.map { TestStrip.new } }
+
+      # context '990' do
+      #   let(:poisoned) { 990 }
+
+      #   it do
+      #     expect(subject[:bottle]).to eq bottles[poisoned]
+      #     expect(subject[:steps]).to eq 3
+      #   end
+      # end
+
+      # TODO: check 573 316
+      context '797 with more bottles that stipes at the last step' do
+        let(:poisoned) { 797 }
+
+        it do
+          expect(subject[:bottle]).to eq bottles[poisoned]
+          expect(subject[:steps]).to eq 3
+        end
+      end
+
+      context '901' do
+        let(:poisoned) { p rand(1000) }
+
+        it do
+          expect(subject[:bottle]).to eq bottles[poisoned]
+          expect(subject[:steps]).to eq 3
+        end
+      end
+    end
+  end
+end
