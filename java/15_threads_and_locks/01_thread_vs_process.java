89add7ceae0ac921193da90bf6f17b84b6d4a4f8
Alex Emelyanov <holyketzer@gmail.com>
2019-11-07 11:57:18 +0300

    Solved 15.1 and 15.2 in Java

---
+++ java/15_threads_and_locks/01_thread_vs_process.java
@@ -0,0 +1,10 @@
+// Process - instance of a program execution
+// OS resources allocated for process
+// Process can address data from another process
+// Inter-process communication - pipes, files, sockets, shared memory, etc.
+// Process switching is expensive
+
+// Threads run inside process
+// Each threads has its own registers and stack
+// Threads can access each other heap data and any process resources
+// Switching Threads is expensive?
